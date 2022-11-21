ecb5643009b7739703ac9bc5a1a15dc0fceb4d78
Matthew Taylor <matthewaptaylor@gmail.com>
2022-11-21 19:01:37 +1300

    Migrated to Apollo Server 4

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,75 @@
 import * as functions from 'firebase-functions';
 import { initializeApp } from 'firebase-admin/app';
-
-// Ensure that the Firebase Admin SDK is initialised
-initializeApp();
-
-import { ApolloServer } from 'apollo-server-cloud-functions';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { json } from 'body-parser';
+import type { Context } from './context';
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
 import { getUser } from './utils/auth';
 import dataSources from './dataSources';
 
-const server = new ApolloServer({
+// Ensure that the Firebase Admin SDK is initialised
+initializeApp();
+
+const expressServer = express();
+
+// Start Apollo server
+const httpServer = http.createServer(expressServer);
+const server = new ApolloServer<Context>({
   typeDefs,
   resolvers,
-  csrfPrevention: true,
-  cache: 'bounded',
-
-  context: async ({ req }) => {
-    const user = req.headers.authorization
-      ? await getUser(req.headers.authorization)
-      : undefined;
-
-    return { user };
-  },
-
-  dataSources,
 
   // eslint-disable-next-line new-cap
-  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
+/**
+ * Handles a Functions request, ensuring that the Apollo server is attached to the Express server.
+ * @param req The Express request.
+ * @param res The Express response.
+ * @returns The express server.
+ */
+const handleRequest = async (
+  req: functions.https.Request,
+  res: functions.Response
+) => {
+  try {
+    await server.start();
+
+    // Add Apollo middleware to express server
+    expressServer.use(
+      '/',
+      cors<cors.CorsRequest>(),
+      json(),
+      expressMiddleware(server, {
+        context: async ({ req }): Promise<Context> => {
+          const user = req.headers.authorization
+            ? await getUser(req.headers.authorization)
+            : undefined;
+
+          // Initialise data sources
+          const ds = dataSources();
+          ds.users.initialize();
+          ds.userExperiences.initialize();
+          ds.courses.initialize();
+
+          return { user, dataSources: ds };
+        },
+      })
+    );
+  } catch (error) {
+    console.log('Function already initialised');
+  }
+
+  return expressServer(req, res);
+};
+
+// Export express server as a Cloud Function
 export const graphql = functions
   .region('australia-southeast1')
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  .https.onRequest(server.createHandler() as any);
+  .https.onRequest(handleRequest);
